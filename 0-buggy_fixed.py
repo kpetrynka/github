5a06b7d1d90bb04c516323360022aafb3897a955
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-11-19 11:35:27 +0200

    First practise (#1)
    * created
    
    * created js
    
    * created py
    
    * fixed problems
    
    * fixed basics

---
+++ 0-buggy_fixed.py
@@ -0,0 +1,6 @@
+# got rid of ";"
+
+first_num = 2
+second_num = 3  # fixed secord to second
+sum = first_num + second_num  # fixed the names of variables
+print(sum)
---
+++ 0-buggy_fixxed.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int first_num = 2;
+  int second_num = 3;  // changed secord to second 
+  int sum = first_num + second_mum;  // changed the names of variables
+  printf("%d\n", sum);
+}
