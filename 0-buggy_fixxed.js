8a99474af7838d380d18a95992f0fee660a523dd
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-11-19 11:45:53 +0200

    corrected to constants

--- 0-buggy_fixxed.js
+++ 0-buggy_fixxed.js
@@ -1,4 +1,4 @@
-let first_num = 2;
-let second_num = 3;  // changed secord to second
-let sum = first_num + second_num; // changed the names of variables
+const first_num = 2;
+const second_num = 3;  // changed secord to second
+const sum = first_num + second_num; // changed the names of variables
 console.log(sum);
