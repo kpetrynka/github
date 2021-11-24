a9bd02578a903cdc5db581ae42197424a285283d
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-11-24 10:53:29 +0200

    corrected cpp file so it works

--- func_sum.cpp
+++ func_sum.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <array>
 using namespace std;
 using std::cout;
-using std::array;
 
-int prices[] = {40,30,50};
-int adding(int x[]) {
+int prices[] = { 40, 30, 50 };
+
+int adding(int x[], int size) {
     int to_pay = 0;
-    for (int i = 0; i<sizeof(x) ; i++) {
+    for (int i = 0; i < size; i++) {
         to_pay += x[i];
     }
-    return to_pay;  
+    return to_pay; 
 }
 
-cout << adding(prices[]);
+int main() {
+    int result = adding(prices, sizeof(prices));
+    cout << result;
+    return 0;
+}
